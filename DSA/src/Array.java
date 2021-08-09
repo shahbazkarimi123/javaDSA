5dd947c19cba788c8d20111be18e1a47f19d9c55
shahbazkarimi123 <shahbazkarimi123@gmail.com>
2021-08-09 08:02:21 +0530

    java dsa

---
+++ DSA/src/Array.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Array {
+    public static void main(String[] args)
+    {
+        int i,j=0;
+        System.out.println("Enter the size of array: ");
+        Scanner cin = new Scanner(System.in);
+        int size = cin.nextInt();
+        int[] arr;
+        arr = new int[size];
+        for(i=0;i<size;i++)
+        {
+            j=i+1;
+            System.out.println("Enter "+(j)+" Element");
+            arr[i] = cin.nextInt();
+            int l;
+        }
+
+
+    }
+}
