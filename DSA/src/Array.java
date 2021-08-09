adad2ba2a6f62fb7396e4562dba1bebe870ad82f
shahbazkarimi123 <shahbazkarimi123@gmail.com>
2021-08-09 12:36:40 +0530

    Array is completed

--- DSA/src/Array.java
+++ DSA/src/Array.java
@@ -2,20 +2,88 @@ import java.util.Scanner;
 public class Array {
     public static void main(String[] args)
     {
-        int i,j=0;
-        System.out.println("Enter the size of array: ");
-        Scanner cin = new Scanner(System.in);
-        int size = cin.nextInt();
         int[] arr;
-        arr = new int[size];
-        for(i=0;i<size;i++)
+        int start=0;
+        int size,pos,val;
+        System.out.println("Enter the size of array.");
+        Scanner cin = new Scanner(System.in);
+        size = cin.nextInt();
+        arr=new int[20];
+        for(int i=0;i<size;i++)
         {
-            j=i+1;
-            System.out.println("Enter "+(j)+" Element");
+            System.out.println("Enter the "+(i+1)+" element: ");
             arr[i] = cin.nextInt();
-            int l;
         }
+        for(int i=0;i<size;i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println("\n");
 
+        //delete in at the position you want
+        System.out.println("Enter the position you want to delete.");
+        pos = cin.nextInt();
+        for(int i=0;i<size;i++)
+        {
+            if(arr[i]==pos)
+            {
+                for(;i<size;i++)
+                {
+                    arr[i]= arr[i+1];
+                }
+            }
+        }
+        size--;
+        for(int i=0;i<size;i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println("\n");
+        //Enter the the value at the position you want
+        System.out.println("Enter the valu you want.: ");
+        val = cin.nextInt();
+        System.out.println("Enter the position you want: ");
+        pos = cin.nextInt();
+        for(int i=0;i<size;i++)
+        {
+            if(i==pos)
+            {
+                int temp;
+                size++;
+                temp=arr[i];
+                arr[i]=val;
+                int j;
+                for(j=++i;j<size;j++)
+                {
+                    int temp1 = arr[j];
+                    arr[j]=temp;
+                    temp=temp1;
+                }
+            }
+
+        }
+        for(int i=0;i<size;i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println("\n");
+        int end=--size;
+        //REversing array
+        int temp3;
+        while(start<end)
+        {
+            temp3=arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp3;
+            start++;
+            end--;
+        }
+        size++;
+        for(int i=0;i<size;i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println("\n");
 
     }
 }
