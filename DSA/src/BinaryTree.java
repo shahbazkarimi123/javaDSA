3a98b6296800986cec4babee2333c596f125df96
shahbazkarimi123 <shahbazkarimi123@gmail.com>
2021-08-16 10:14:37 +0530

    stack and queue

--- DSA/src/BinaryTree.java
+++ DSA/src/BinaryTree.java
@@ -32,24 +32,14 @@ public class BinaryTree {
 
     }
 
-    public void preOrder(TreeNode root)
-    {
-        if(root == null)
-        {
-            return;
-        }
 
-        System.out.print(root.data+" ");
-        preOrder(root.left);
-        preOrder(root.right);
-    }
 
 
     public static void main(String[] args)
     {
         BinaryTree bt = new BinaryTree();
         bt.createBinaryTree();
-        bt.preOrder(bt.root);
+
 
     }
 }
