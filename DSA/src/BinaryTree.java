e7c008f335534288a7cae67f965c77dcb0a18f69
shahbazkarimi123 <shahbazkarimi123@gmail.com>
2021-08-16 10:13:46 +0530

    stack and queue

---
+++ DSA/src/BinaryTree.java
@@ -0,0 +1,55 @@
+import com.sun.source.tree.Tree;
+
+public class BinaryTree {
+    private TreeNode root;
+    private class TreeNode
+    {
+        private int data;
+        private TreeNode left;
+        private TreeNode right;
+
+        public TreeNode(int data)
+        {
+            this.data = data;
+            left = null;
+            right = null;
+        }
+    }
+
+    public void createBinaryTree()
+    {
+        TreeNode first = new TreeNode(9);
+        TreeNode second = new TreeNode(2);
+        TreeNode third = new TreeNode(3);
+        TreeNode fourth = new TreeNode(4);
+
+
+        root = first;
+        first.left = second;
+        first.right = third;
+        second.left = fourth;
+
+
+    }
+
+    public void preOrder(TreeNode root)
+    {
+        if(root == null)
+        {
+            return;
+        }
+
+        System.out.print(root.data+" ");
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+
+
+    public static void main(String[] args)
+    {
+        BinaryTree bt = new BinaryTree();
+        bt.createBinaryTree();
+        bt.preOrder(bt.root);
+
+    }
+}
